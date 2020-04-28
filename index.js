93db360e0973f31a9daa2d3cf2163980cb12ef91
moisac <moisaclaudiu2309@gmail.com>
2020-04-28 22:04:17 +0300

    Upload to use on heroku

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const morgan = require('morgan')
 const app = express()
+const cors = require('cors')
 
 
 app.use(express.json())
+app.use(cors())
 
 morgan.token('body', function (req, res) { 
   return JSON.stringify(req.body) 
@@ -96,7 +98,7 @@ let phonebook = [
 
   
 
-  const PORT = 3001
+  const PORT = process.env.PORT || 3001
 
   app.listen(PORT, () => {
       console.log(`Server running on port ${PORT}`)
