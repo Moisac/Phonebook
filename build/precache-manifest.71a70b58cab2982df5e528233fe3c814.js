5dc685e059f42eaea9cb46094581ccc7eebad648
moisac <moisaclaudiu2309@gmail.com>
2020-04-29 21:00:16 +0300

    react frontend files was added

---
+++ build/precache-manifest.71a70b58cab2982df5e528233fe3c814.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18c16ec2d8d1ca8b8efb46479e31b897",
+    "url": "/index.html"
+  },
+  {
+    "revision": "451544b7a93a810ac2ee",
+    "url": "/static/css/main.d8e57eaa.chunk.css"
+  },
+  {
+    "revision": "eecea025062f36901e18",
+    "url": "/static/js/2.82f48395.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.82f48395.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "451544b7a93a810ac2ee",
+    "url": "/static/js/main.5f35601d.chunk.js"
+  },
+  {
+    "revision": "cbe135e57de162cd240f",
+    "url": "/static/js/runtime-main.d0b0a789.js"
+  }
+]);
---
+++ build/precache-manifest.9460f422826f299656c730ff3e48ab3e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2df086c0e4fc682d6908ef76ed9aa3d8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "64517d73f4d5e91f5eb7",
+    "url": "/static/css/main.d8e57eaa.chunk.css"
+  },
+  {
+    "revision": "eecea025062f36901e18",
+    "url": "/static/js/2.82f48395.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.82f48395.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64517d73f4d5e91f5eb7",
+    "url": "/static/js/main.6546202c.chunk.js"
+  },
+  {
+    "revision": "cbe135e57de162cd240f",
+    "url": "/static/js/runtime-main.d0b0a789.js"
+  }
+]);
